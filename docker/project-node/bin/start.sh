e5a6812ca56a5265025751819d5c330510089814
coso <wutongci@hotmail.com>
2019-04-12 14:56:45 +0800

    增加部署官网

--- docker/project-node/bin/start.sh
+++ docker/project-node/bin/start.sh
@@ -15,6 +15,7 @@ function git_pull_or_clone()
     cd $CURR_DIR
 }
 
+# 部署admin.bullteam.cn
 mkdir -p /data/src/web
 repo_url="git@github.com:bullteam/zeus-ui.git"
 git_pull_or_clone ${repo_url} "/data/src/web/admin.bullteam.cn" master
@@ -22,3 +23,7 @@ git_pull_or_clone ${repo_url} "/data/src/web/admin.bullteam.cn" master
 cd /data/src/web/admin.bullteam.cn/
 npm install && npm cache clean --force
 npm run build:prod
+
+#部署www.bullteam.cn
+repo_url="git@github.com:bullteam/website.git"
+git_pull_or_clone ${repo_url} "/data/src/web/www.bullteam.cn" master
